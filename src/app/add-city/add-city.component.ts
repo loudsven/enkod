a8916ed0c72f38c503d3800dbbf87d9ad3f720c9
loudsven <loudsven@tutanota.com>
2025-03-08 12:42:14 +0300

    add new city form

--- src/app/add-city/add-city.component.ts
+++ src/app/add-city/add-city.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-city',
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule, ReactiveFormsModule, HeaderComponent],
   templateUrl: './add-city.component.html',
   styleUrl: './add-city.component.css',
 })
-export class AddCityComponent {}
+export class AddCityComponent {
+  cityForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    image: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+  });
+
+  onSubmit() {
+    console.log(this.cityForm.value);
+  }
+}
