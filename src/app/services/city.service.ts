15ee2b8142278fc0ee4f42fd96606a5f3e5822af
loudsven <loudsven@tutanota.com>
2025-03-08 05:56:55 +0300

    add cities data

---
+++ src/app/services/city.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { City } from '../models/city.model';
+import { cities } from '../data/cities';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CityService {
+  private citiesSubject = new BehaviorSubject<City[]>(cities);
+  cities$ = this.citiesSubject.asObservable();
+
+  constructor() {}
+
+  toggleFavorite(cityId: number): void {
+    const cities = this.citiesSubject.value;
+    const city = cities.find((c) => c.id === cityId);
+    if (city) {
+      city.favorite = !city.favorite;
+      this.citiesSubject.next([...cities]);
+    }
+  }
+}
